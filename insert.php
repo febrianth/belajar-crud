4017ef65a75dd004a29f301ec890a0582ea83ea8
febrianth <pekmbiee14@gmail.com>
2022-01-26 11:58:16 +0700

    form insert

--- insert.php
+++ insert.php
@@ -1,11 +1,23 @@
 <?php
+$firstname="Febrian";
+$email="pekmbiee@gmail.com";
+$lastname = "Hardiyanto";
+if (!empty($_POST)){
+    $firstname=$_POST["firstname"];
+    $email=$_POST["email"];
+    $lastname=$_POST["lastname"];
+} else {
+    die("input harus lewat form");
+}
 include "config.php";
 $sql = "INSERT INTO MyGuests ( firstname, lastname, email )
-VALUES ( 'Febrian', 'Hardiyanto', 'pekmbiee14@gmail.com' )" ;
-if (mysqli_query( $conn, $sql )) {
+VALUES (?,?,?)" ;
+$stmt = mysqli_prepare($conn,$sql);
+mysqli_stmt_bind_param($stmt, "sss", $firstname,$lastname,$email);
+if (mysqli_stmt_execute($stmt)) {
     echo "new record created successfully" ;
 } else {
-    echo "error: " .$sql ."<br>" . $mysqli_error($conn);
+    echo "error: " .$sql ."<br>" . mysqli_error($conn);
 }
 mysqli_close($conn);
 ?>
